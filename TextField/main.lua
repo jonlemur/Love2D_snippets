23e3fdc5506cabd42f21c38960ca21a10de3ab75
jonlemur <jonpirat@gmail.com>
2017-11-19 23:29:16 +0100

    textfields update

--- TextField/main.lua
+++ TextField/main.lua
@@ -1,3 +1,5 @@
+require ("textField")
+
 local centerX = 0
 local centerY = 0
 local DEFAULT_WINDOW_WIDTH = 960
@@ -5,25 +7,58 @@ local DEFAULT_WINDOW_HEIGHT = 540
 local DEFAULT_ZOOM  = 2
 
 function love.load()
-love.graphics.setDefaultFilter( "nearest", "nearest", 1 ) -- set for pixel perfect rendering
-zoom = DEFAULT_ZOOM -- how zoomed in is the picture
-love.window.setMode( DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, {resizable=true, vsync=false, minwidth=400, minheight=300})
-centerX = love.graphics.getWidth( ) / 2 / zoom
-centerY = love.graphics.getHeight( ) / 2 / zoom
+	love.graphics.setDefaultFilter( "nearest", "nearest", 1 ) -- set for pixel perfect rendering
+	love.keyboard.setKeyRepeat(true) -- so keys may be held
+	zoom = DEFAULT_ZOOM -- how zoomed in is the picture
+	love.window.setMode( DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, {resizable=true, vsync=false, minwidth=400, minheight=300})
+	centerX = love.graphics.getWidth( ) / 2 / zoom
+	centerY = love.graphics.getHeight( ) / 2 / zoom
 
-sheet = love.graphics.newImage("sheet01.png") --example image 
-sCenterX = sheet:getWidth() / 2
-sCenterY = sheet:getHeight() / 2
+	sheet = love.graphics.newImage("img/sheet01.png") --example image 
+	sCenterX = sheet:getWidth() / 2
+	sCenterY = sheet:getHeight() / 2
+	
+	textField1 = textField:new("tfNameInput",centerX-170,centerY-90)
+	textField2 = textField:new("tfNameInput2",centerX-170,centerY-60)
+
 end
 
+
+function love.mousepressed(x, y, button, istouch)
+	textField1:clicked(x,y, zoom)
+	textField2:clicked(x,y, zoom)
+end
+
+
+function love.textinput(t)
+    textField1:typing(t)
+	textField2:typing(t)
+end
+
+
+function love.keypressed(key)
+    if key == "backspace" then
+		
+        textField1:deleting()
+		textField2:deleting()
+    end
+end
+
+
 function love.resize(w, h)
-zoom = w/DEFAULT_WINDOW_WIDTH * DEFAULT_ZOOM
-centerX = love.graphics.getWidth( ) / 2 / zoom
-centerY = love.graphics.getHeight( ) / 2 / zoom
+	zoom = w/DEFAULT_WINDOW_WIDTH * DEFAULT_ZOOM
+	centerX = love.graphics.getWidth( ) / 2 / zoom
+	centerY = love.graphics.getHeight( ) / 2 / zoom
+	
+	textField1:reposition(centerX-170,centerY-90)
 end
 
 function love.draw()
 	love.graphics.scale(zoom)
 	love.graphics.setBackgroundColor( 50, 50, 50 )
 	love.graphics.draw(sheet, centerX, centerY, 0, 1, 1, sCenterX,sCenterY)	
+	love.graphics.draw(textField1.bg, textField1.x,textField1.y)
+	love.graphics.print(textField1.text, textField1.x+5,textField1.y+5)
+	love.graphics.draw(textField2.bg, textField2.x,textField2.y)
+	love.graphics.print(textField2.text, textField2.x+5,textField2.y+5)
 end
---
+++ TextField/textField.lua
@@ -0,0 +1,63 @@
+textField = {}
+textField.__index = textField
+
+local utf8 = require("utf8")
+
+function textField:new(name, x, y)
+	local tF = {}
+	setmetatable(tF, textField)
+	tF.name = name
+	tF.x=x
+	tF.y=y
+	tF.bg = love.graphics.newImage("img/texField.png")
+	tF.text= "Text"
+	tF.selected = false
+	return tF
+end
+
+--Use this for resize events
+function textField:reposition(x,y)
+	self.x = x
+	self.y = y
+end
+
+function textField:typing(t)
+	if self.selected then
+	self.text = self.text .. t
+	end
+end
+
+function textField:deleting()
+        local byteoffset = utf8.offset(self.text, -1)
+ 
+        if byteoffset and self.selected then
+            self.text = string.sub(self.text, 1, byteoffset - 1)
+        end
+end
+
+function textField:clicked(mouseX, mouseY, zoom)
+	--width and height of the textfield
+	local rBound = 80
+	local lBound = 20
+	rBound = rBound + self.x
+	lBound = lBound + self.y
+
+	local btnX = self.x
+	btnX = btnX * zoom
+	local btnY = self.y
+	btnY = btnY * zoom
+	
+	rBound = rBound * zoom
+	lBound = lBound * zoom
+	
+	if mouseX> btnX and mouseX<rBound then
+		if mouseY> btnY and mouseY<lBound then
+			self.selected = true
+			print(self.name .. " selected")	
+		else
+			self.selected = false
+			print(self.name .. " deselected")
+		end
+	end
+	
+end
